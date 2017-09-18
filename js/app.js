f1dce13356b987657f15b0e5a40725570bbc5c8e
M. Sophia LeBlanc <mle269@uky.edu>
2017-09-18 19:42:10 -0400

    Create processData function to get rates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function () {
 
     // mapbox access token for sophia58 account
-    L.mapbox.accessToken = 'pk.eyJ1Ijoicmdkb25vaHVlIiwiYSI6Im5Ua3F4UzgifQ.PClcVzU5OUj17kuxqsY_Dg';
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic29waGlhNTgiLCJhIjoiY2o2OWliZThsMHNwcDMycnBrcnEyNHlkZiJ9._d2dOKGl4oPWzUxOSnMgfA';
 
     // create the Leaflet map using mapbox.light tiles
     var map = L.mapbox.map('map', 'mapbox.light', {
@@ -13,8 +13,77 @@
         //        maxBounds: L.latLngBounds([-6.22, 27.72], [5.76, 47.83])
     });
 
+    //Load in tract and income data
+    $.getJSON("data/tract_48201_and_incomedata.geojson", function (tract) {
+
+        // counties is accessible here
+        console.log(tract);
+        drawMap(tract);
+        processData(tract);
+
+    });
+
+    function processData(tract) {
+
+        var rates = [];
+        //console.log('tract: ', tract);
+        tract.features.map(function (tractData) {
+
+            for (var prop in tractData.properties) {
+
+                //                        if ($.isNumeric(county.properties[prop]) && prop != 'GEOID' && prop != 'GEOID') {
+
+                rates.push(Number(tractData.properties[prop]));
+                console.log('rates: ', rates);
+                //                        }
+            }
+        });
+    }
 
 
+
+
+    function drawMap(tract, colorize) {
+
+        // create Leaflet object with geometry data and add to map
+        var dataLayer = L.geoJson(tract, {
+            style: function (feature) {
+                return {
+                    color: 'black',
+                    weight: 1,
+                    fillOpacity: 1,
+                    fillColor: '#1f78b4'
+                };
+            }
+        }).addTo(map);
+    }
+
+
+    //Dynamic update map function
+    function updateMap(dataLayer, colorize, currentYear) {
+
+        //  loop through each county layer
+        dataLayer.eachLayer(function (layer) {
+
+            // shortcut reference for layer properties
+            var props = layer.feature.properties;
+
+            // set the fill color of layer based on its normalized data value using break values in colorize
+            layer.setStyle({
+                fillColor: colorize(Number(props[currentYear]))
+            });
+
+            // assemble string sequence of info for tooltip
+            var tooltipInfo = "<b>" + props["NAME"] + " </b></br>" + props[currentYear] +
+                "% Unemployment Rate"
+
+            // bind a tooltip to layer with county-specific information
+            layer.bindTooltip(tooltipInfo, {
+                sticky: true
+            });
+
+        });
+    }
 
 
 })();
