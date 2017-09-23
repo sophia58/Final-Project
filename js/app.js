09d4fb1364205a91ca674961ee62f7b0ff2b068c
M. Sophia LeBlanc <mle269@uky.edu>
2017-09-23 13:06:54 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function () {
 
+
     // mapbox access token for sophia58 account
     L.mapbox.accessToken = 'pk.eyJ1Ijoic29waGlhNTgiLCJhIjoiY2o2OWliZThsMHNwcDMycnBrcnEyNHlkZiJ9._d2dOKGl4oPWzUxOSnMgfA';
 
@@ -12,6 +13,8 @@
         zoomControl: true
     });
 
+    // set global variables mapped attribute, and normalizing attribute 
+    var attributeValue = "total";
 
 
     //Load in tract and rainfall data
@@ -48,10 +51,11 @@
                 return L.circleMarker(latlng, bubble(rainfall)); //pass rainfall to bubble function to get
             } // circle options
         }).addTo(map);
-        // updateMap(dataLayer, colorize, '0');
+        updateMap(tract, rainfall);
 
     }
 
+    //style each gage site
     function bubble(pointData) {
         var circleOptions = {
             fillColor: "brown",
@@ -59,56 +63,64 @@
             weight: .8,
             opacity: 1,
             fillOpacity: 0.8,
-            radius: 9
+            radius: 5,
         };
         return circleOptions;
     }
 
-
-
-    //options for creating circleMarkers from GeoJson point data
-    var options = {
-        pointToLayer: function (feature, ll) {
-            return L.circleMarker(ll, {
-                opacity: 3,
-                weight: 1,
-                fillOpacity: .7,
-                fillColor: '#a6bddb',
-                color: 'grey'
-
-            })
-        }
+    //calculate the radius
+    function calcRadius(val) {
+        var radius = Math.sqrt(val / Math.PI);
+        console.log("\n radius :", radius);
+        return radius * 10; // adjust .5 as a scale factor
     }
 
+    //resize the circle radius based rainfall amount
+    function resizeCircles(rainfall) {
+        rainfall.eachLayer(function (layer) {
+            var radius = calcRadius(Number(layer.feature.properties.total));
+            layer.setRadius(radius);
+        }).addTo(map);
 
-
+    }
 
 
     //Dynamic update map function
-    function updateMap(dataLayer, colorize, income) {
+    function updateMap(tract, rainfall) {
 
-        //  loop through each tract layer
-        dataLayer.eachLayer(function (layer) {
+        // get the class breaks for the initial data attribute
+        var breaks = getClassBreaks(tract);
 
-            // shortcut reference for layer properties
-            var props = layer.feature.properties;
-            // console.log(layer.feature.properties.income2017);
+        // draw the legend now that we have the classifications
+        updateLegend(breaks);
 
-            // set the fill color of layer based on its normalized data value using break values in colorize
-            layer.setStyle({
-                fillColor: colorize(Number(props.income2017))
-            });
-
-            // assemble string sequence of info for tooltip
-            var tooltipInfo = "<b>" + "Household Income: " + " </b></br>" + "$" + props.income2017.toLocaleString()
-
-            // bind a tooltip to layer with county-specific information
-            layer.bindTooltip(tooltipInfo, {
-                sticky: true
-            });
-
-        });
+        //        // loop through each county layer
+        //        dataLayer.eachLayer(function (layer) {
+        //
+        //            // shortcut reference for layer properties
+        //            var props = layer.feature.properties;
+        //
+        //            // set the fill color of layer based on its normalized data value
+        //            layer.setStyle({
+        //                fillColor: "yellow"
+        //                //                fillColor: getColor(props[attributeValue] / props[normValue], breaks)
+        //            });
+        //
+        //            // assemble string sequence of info for tooltip
+        //            var tooltipInfo = "test"
+        //
+        //            //                "<b>" + props["NAME"] + " County</b></br>" +
+        //            //                labels[attributeValue] + ": " + ((props[attributeValue] /
+        //            //                    props[normValue]) * 100).toLocaleString() + "%"
+        //
+        //            // bind a tooltip to layer with county-specific information
+        //            layer.bindTooltip(tooltipInfo, {
+        //                sticky: true
+        //            });
+        //
+        //        });
     }
+
 
 
     //drawLegend
@@ -153,21 +165,47 @@
 
     }
 
-    //calculate the radius
-    function calcRadius(val) {
-        var radius = Math.sqrt(val / Math.PI);
-        return radius * 5; // adjust .5 as a scale factor
+    function getClassBreaks(dataLayer) {
+
+        // create empty Array for storing values
+        var values = [];
+
+        // loop through all the counties
+        dataLayer.eachLayer(function (layer) {
+            var value = layer.feature.properties[attributeValue];
+            values.push(value); // push the normalized value for each layer into the Array
+        });
+
+        // determine similar clusters
+        var clusters = ss.ckmeans(values, 5);
+
+        // create an array of the lowest value within each cluster
+        var breaks = clusters.map(function (cluster) {
+            return [cluster[0], cluster.pop()];
+        });
+
+        //return array of arrays, e.g., [[0.24,0.25], [0.26, 0.37], etc]
+        return breaks;
+
     }
 
-    //resize the circle radius based rainfall amount
-    function resizeCircles(rainfall) {
-        rainfall.eachLayer(function (layer) {
-            var radius = calcRadius(Number(layer.feature.properties.total));
-            layer.setRadius(radius);
-        }).addTo(map);
+    function getColor(d, breaks) {
+        // function accepts a single normalized data attribute value
+        // and uses a series of conditional statements to determine which
+        // which color value to return to return to the function caller
 
+        if (d <= breaks[0][1]) {
+            return '#f1eef6';
+        } else if (d <= breaks[1][1]) {
+            return '#bdc9e1';
+        } else if (d <= breaks[2][1]) {
+            return '#74a9cf';
+        } else if (d <= breaks[3][1]) {
+            return '#2b8cbe'
+        } else if (d <= breaks[4][1]) {
+            return '#045a8d'
+        }
     }
-
 
 
 
